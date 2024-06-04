85dcd4610bc2065e8c809627e57c49bc24a87061
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2024-06-04 09:49:53 +0200

    intial project

---
+++ social-information-api/src/main/java/com/example/socialinformationapi/SocialInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialinformationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialInformationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialInformationApiApplication.class, args);
+    }
+
+}
---
+++ social-information-api/src/test/java/com/example/socialinformationapi/SocialInformationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialinformationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialInformationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
